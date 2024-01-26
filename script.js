8422652eb8e7cc4021013974cfd583867922f29a
JVN-010 <ax400704@gmail.com>
2024-01-26 17:48:45 +1100

    committed in Js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(100);
+console.log("Hello World");
+let x = 500;
+console.log(x);
+let y = x * x;
+console.log(y);
+console.table({ name: "Jeewan", address: "Sydney", contact: 1234567890 });
